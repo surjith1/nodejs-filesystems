f6b1319498a748e38c0b848f2bec121646a899ae
surjith1 <rvdsurjith@gmail.com>
2022-06-08 23:56:28 +0530

    dotenv added

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import express from 'express';
 import * as fs from 'node:fs';
+import dotenv from'dotenv';
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 let text=`Hi Welcome to Port ${PORT}`;
 let date = Date();
-let result=fs.writeFile("current-data-time.txt",date,(err)=> {
+fs.writeFile("current-data-time.txt",date,(err)=> {
     console.log("Completed writing!")
 })
+fs.readFile("./current-data-time.txt", "utf8", (err,data)=> {
+    if (err) {
+        console.log("something went wrong",err)
+    }
+    else {
+        console.log(data);
+    }
+});
 app.get('/', (req,res)=> {
     res.send(text);
 })
+
 
 app.listen(PORT,()=>console.log(`port Running on ${PORT}`));
